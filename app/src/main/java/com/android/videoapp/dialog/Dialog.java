485d7da0a956c4d1f89d62485654eee72c9de1fb
SHIFULMAHMUD <shifulmahmudkhan@gmail.com>
2021-11-07 14:42:40 +0600

    Video Call Integration using Twilio

---
+++ app/src/main/java/com/android/videoapp/dialog/Dialog.java
@@ -0,0 +1,35 @@
+package com.android.videoapp.dialog;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.widget.EditText;
+
+import com.android.videoapp.R;
+
+public class Dialog {
+
+    public static AlertDialog createConnectDialog(
+            EditText participantEditText,
+            DialogInterface.OnClickListener callParticipantsClickListener,
+            DialogInterface.OnClickListener cancelClickListener,
+            Context context) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+        alertDialogBuilder.setIcon(R.drawable.ic_video_call_white_24dp);
+        alertDialogBuilder.setTitle("Connect to a Room");
+        alertDialogBuilder.setPositiveButton("Connect", callParticipantsClickListener);
+        alertDialogBuilder.setNegativeButton("Cancel", cancelClickListener);
+        alertDialogBuilder.setCancelable(false);
+
+        setRoomNameFieldInDialog(participantEditText, alertDialogBuilder);
+
+        return alertDialogBuilder.create();
+    }
+
+    private static void setRoomNameFieldInDialog(
+            EditText roomNameEditText, AlertDialog.Builder alertDialogBuilder) {
+        roomNameEditText.setHint("Room Name");
+        alertDialogBuilder.setView(roomNameEditText);
+    }
+}
